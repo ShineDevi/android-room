d09327b671bd68226b413747ad845b7c48710d35
ShineDevi <shinedevi02@gmail.com>
2022-10-23 21:07:37 +0700

    Membuat room

---
+++ README.md
@@ -0,0 +1,10 @@
+# LAPORAN PRAKTIKUM MEMBUAT ROOM
+NAMA: SHINE DEVI OKTAVIANA R S P <BR>
+KELAS: TI 3C <BR>
+NIM: 2041720065
+## 1. Tampilan awal
+![tampilan awal](ss/awal.jpeg)
+## 2. Menambahkan data
+![tambah data](ss/add.jpeg)
+## 3. Hasil menambahkan data
+![hasil](ss/hasil.jpeg)
---
+++ app/src/main/java/jti/polinema/ac/id/room/WordDao.java
@@ -0,0 +1,20 @@
+package jti.polinema.ac.id.room;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+}
